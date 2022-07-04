2674fb68f46811afa80eb710b7e275abdd180db4
pmentropy <justin.duplain@gmail.com>
2022-07-04 12:28:52 -0400

    adds correct page numbers to a set

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,27 +1,43 @@
 export const DOTS = "...";
 
 /**
- * Returns pagination data based on currently selected details
+ * Function: Returns pagination data based on currently selected details
+ * Uses a set to avoid duplicates, then performas a map on the set to insert ellipses (DOTS)
+ * where necessary.
  */
 function usePagination({ currentPage, pageSize, totalCount }) {
-  const max = Math.ceil(totalCount / pageSize);
-  const pages = [];
+  //calculates the last page number
+  const lastPage = Math.ceil(totalCount / pageSize);
+  //
+  const pageSet = new Set([1]);
 
-  if (max <= 4) {
-    for (let i = 1; i <= max; i++) {
-      pages.push(i);
+  if (lastPage <= 4) {
+    // if 4 pages or less, add all of them
+    for (let i = 1; i <= lastPage; i++) {
+      pageSet.add(i);
     }
-  } else {
-    pages.push(1);
-    pages.push(DOTS);
-    pages.push(currentPage - 1);
-    pages.push(currentPage);
-    pages.push(currentPage + 1);
-    pages.push(DOTS);
-    pages.push(max);
+    //returns the set spread to an array
+    return [...pageSet];
   }
-  console.log(pages);
-  return pages;
+  // if 5 or more pages, this builds the set based on current page
+  if (currentPage === 1) {
+    //adds two right siblings
+    pageSet.add(2);
+    pageSet.add(3);
+  } else if (currentPage === lastPage) {
+    //adds the two left siblings
+    pageSet.add(currentPage - 1);
+    pageSet.add(currentPage - 2);
+  } else {
+    //adds left sibling, current page, and right sibling
+    pageSet.add(currentPage - 1);
+    pageSet.add(currentPage);
+    pageSet.add(currentPage + 1);
+  }
+  //adds the last page to the set
+  pageSet.add(lastPage);
+  // transforms the set to an array and inserts ellipses
+  return [...pageSet];
 }
 
 export default usePagination;
